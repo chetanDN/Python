c6e14b38371a30b2cd8b3fc83e0697a1aaac5b23
chetanDN <chetandevanaik@gmail.com>
2017-01-09 17:14:29 +0530

    Update 5.Nested_Lists.py

--- 2.Basic Data Types/5.Nested_Lists.py
+++ 2.Basic Data Types/5.Nested_Lists.py
@@ -63,3 +63,12 @@ if __name__ == '__main__':
         if marks == second_highest:
             print(name)
     
+"""
+#using lists only as required nested lists not dictionary
+n = int(input())
+marksheet = [[input(), float(input())] for _ in range(n)]
+
+second_highest = sorted(list(set([marks for name, marks in marksheet])))[1]
+
+print('\n'.join([a for a,b in sorted(marksheet) if b == second_highest]))
+"""
