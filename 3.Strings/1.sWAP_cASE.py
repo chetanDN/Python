34d34e24067634404be9e93b07da91c5c3dd7563
chetanDN <chetandevanaik@gmail.com>
2017-01-10 18:17:38 +0530

    Create 1.sWAP_cASE.py

---
+++ 3.Strings/1.sWAP_cASE.py
@@ -0,0 +1,40 @@
+"""
+You are given a string . Your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
+
+For Example:
+
+Www.HackerRank.com → wWW.hACKERrANK.COM
+Pythonist 2 → pYTHONIST 2
+Input Format
+
+A single line containing a string .
+
+Constraints
+
+
+Output Format
+
+Print the modified string .
+
+Sample Input
+
+HackerRank.com presents "Pythonist 2".
+Sample Output
+
+hACKERrANK.COM PRESENTS "pYTHONIST 2".
+"""
+
+def swap_case(s):
+    str2 = ''
+    for ch in s:
+        if 'a'<=ch<='z' or 'A'<=ch<='Z':
+            str2 += chr(ord(ch)^32)            #ord() char to ascii int ; chr()-> ascii int to char
+        else:
+            str2 += ch
+            
+    return str2
+    
+    if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
