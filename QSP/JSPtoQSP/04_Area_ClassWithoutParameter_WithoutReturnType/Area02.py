08c6e324ae5dd4018a53ffc98ce50da371278010
chetanDN <chetandevanaik@gmail.com>
2019-01-29 14:20:05 +0530

    newely added

---
+++ QSP/JSPtoQSP/04_Area_ClassWithoutParameter_WithoutReturnType/Area02.py
@@ -0,0 +1,9 @@
+class AreaCalculator(object):
+    @staticmethod
+    def find_area():
+        pass
+
+    
+class Main:
+    AreaCalculator.find_area()
+
---
+++ QSP/JSPtoQSP/04_Area_ClassWithoutParameter_WithoutReturnType/Area03.py
@@ -0,0 +1,8 @@
+def find_area(x, y, d):
+    area = ((x + y) / 2) * d  # Area_of_trapezoid
+    return area
+
+
+area_of_trapezoid = find_area(10, 20, 5)
+print("area of trapezoid is :", area_of_trapezoid)
+
