ac89e1f6e4ec0435864472dbda5ea03ecb3ab08d
chetanDN <chetandevanaik@gmail.com>
2019-01-29 14:49:47 +0530

    Create Area05.py

---
+++ QSP/JSPtoQSP/04_Area_ClassWithoutParameter_WithoutReturnType/Area05.py
@@ -0,0 +1,10 @@
+class FindArea:
+    @staticmethod
+    def find_square_area():
+        x = 10
+        area = x * x  # area of the square
+        print("area of square :", area)
+
+
+class Main:
+    FindArea.find_square_area()
