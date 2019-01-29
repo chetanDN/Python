444feab3672fe1fb33a56790381c70be6d05069a
chetanDN <chetandevanaik@gmail.com>
2019-01-29 14:26:17 +0530

    updated correctly

--- QSP/JSPtoQSP/04_Area_ClassWithoutParameter_WithoutReturnType/Area02.py
+++ QSP/JSPtoQSP/04_Area_ClassWithoutParameter_WithoutReturnType/Area02.py
@@ -1,9 +1,12 @@
 class AreaCalculator(object):
     @staticmethod
     def find_area():
-        pass
+        width = 10
+        length = 20
+        area_of_rectangle = width * length
+        print("area_of_rectangle : ", area_of_rectangle)
 
-    
+
 class Main:
     AreaCalculator.find_area()
 
--- QSP/JSPtoQSP/04_Area_ClassWithoutParameter_WithoutReturnType/Area03.py
+++ QSP/JSPtoQSP/04_Area_ClassWithoutParameter_WithoutReturnType/Area03.py
@@ -1,8 +1,10 @@
-def find_area(x, y, d):
-    area = ((x + y) / 2) * d  # Area_of_trapezoid
-    return area
+class AreaCalculator(object):
+    @staticmethod
+    def find_area():
+        x, y, d = 10, 20, 5
+        area_of_trapezoid = ((x + y) / 2) * d  # Area_of_trapezoid
+        print("area of trapezoid is :", area_of_trapezoid)
 
 
-area_of_trapezoid = find_area(10, 20, 5)
-print("area of trapezoid is :", area_of_trapezoid)
-
+class Main:
+    AreaCalculator.find_area()
